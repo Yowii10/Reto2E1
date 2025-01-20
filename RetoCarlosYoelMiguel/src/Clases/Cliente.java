f32adc51413a5fd6095ba5ceb64d882b90fd83f8
Yowii10 <yoeliranzo@gmail.com>
2025-01-20 13:23:23 +0100

    Creada clase cliente y añadido metodos

--- RetoCarlosYoelMiguel/src/Clases/Cliente.java
+++ RetoCarlosYoelMiguel/src/Clases/Cliente.java
@@ -1,5 +1,32 @@
 package Clases;
 
-public class Cliente {
+import java.util.List;
 
+public class Cliente {
+private String nombre;
+private List<Tienda> compra;
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public List<Tienda> getCompra() {
+	return compra;
+}
+public void setCompra(List<Tienda> compra) {
+	this.compra = compra;
+}
+public Cliente(String nombre, List<Tienda> compra) {
+	super();
+	this.nombre = nombre;
+	this.compra = compra;
+}
+public Cliente() {
+	super();
+}
+@Override
+public String toString() {
+	return "Cliente [nombre=" + nombre + ", compra=" + compra + "]";
+}
 }
